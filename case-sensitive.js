1ecb064b56674d73ff41754816e9dabda1c4dbd5
enilsonfilho <enilsonfilho.dev@hotmail.com>
2022-01-15 11:41:10 -0300

    Case sensitive

---
+++ case-sensitive.js
@@ -0,0 +1,15 @@
+const minhaVar = 1;
+const MinhaVar = "texto";
+const minhavar = "3";
+const MINHAVAR = 2;
+
+console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
+
+let input = null;
+let input2;
+
+console.log(input); // null
+console.log(input2); // undefined
+
+console.log(null == undefined); // true
+console.log(null === undefined); // false
