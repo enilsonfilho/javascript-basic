57630e42ae091602831c0f839d1f404c507510e1
enilsonfilho <enilsonfilho.dev@hotmail.com>
2022-01-15 12:26:46 -0300

    Trabalhando com constantes

---
+++ var-let-const.js
@@ -0,0 +1,33 @@
+// var
+
+//var altura = 5;
+//var comprimento = 7;
+
+//var area = altura * comprimento;
+//console.log(area);
+//var area;
+
+// let
+//let forma = 'retângulo';
+//let altura = 5;
+//let comprimento = 7;
+
+//if (forma === "retângulo") {
+//    area = altura * comprimento;
+//} else {
+//    area = altura * comprimento / 2;
+//}
+//console.log(area);
+
+const forma = 'quadrado';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma === 'quadrado') {
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area)
