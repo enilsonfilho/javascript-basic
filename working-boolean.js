c4bf78d54d97712ba3fa6e96210f6efbcec301ab
enilsonfilho <enilsonfilho.dev@hotmail.com>
2022-01-15 12:56:28 -0300

    Trabalhando com booleanos

---
+++ working-boolean.js
@@ -0,0 +1,30 @@
+// boolean
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+// truthy oufalsy
+
+//0 => false
+//1 => true
+
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);
+
+// undefined
+// null ==> vazio ou nada
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+let numero = 3;
+let texto ="Alura";
+
+console.log(typeof numero);
+console.log(typeof texto);
+console.log(typeof minhaVar);
+console.log(typeof varNull);
