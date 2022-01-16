5db14c7a1e67d72641e28f9ba8ebfe5edbe149e3
enilsonfilho <enilsonfilho.dev@hotmail.com>
2022-01-16 18:05:59 -0300

    Trabalhando com conversões

---
+++ conversoes.js
@@ -0,0 +1,12 @@
+// tipo de dado
+// booleanos
+
+// conversão implícita
+const numero = 456;
+const numeroString = "456";
+
+//Number()
+//String()
+console.log(numero + Number(numeroString));
+
+// conversão explícita
