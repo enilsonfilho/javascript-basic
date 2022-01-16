c2702e2f7b0179e794f2a9c16495e5e3b63e4886
enilsonfilho <enilsonfilho.dev@hotmail.com>
2022-01-16 18:48:09 -0300

    Trabalhando com arrow function

---
+++ arrow-function.js
@@ -0,0 +1,18 @@
+function apresentar(nome) {
+    return `Meu nome é ${nome}`;
+}
+
+// Arrow function
+const apresentarArrow = nome => `Meu nome é ${nome}`;
+const soma = (num1, num2) => num1 + num2;
+
+//Arrow function com + de 1 linha de instrução
+const somanumerosPequenos = (num1, num2) => {
+    if (num1 || num2 > 10) {
+        return "Somente números de 1 a 9!";
+    } else {
+        return num1 + num2;
+    }
+}
+
+// Hoisting: arrow function se comporta como expressão
