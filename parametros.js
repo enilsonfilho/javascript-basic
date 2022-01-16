a1c2b744a1cb4abfc268c4febdee7f15a2657b24
enilsonfilho <enilsonfilho.dev@hotmail.com>
2022-01-16 18:25:13 -0300

    Trabalando com parâmetros

---
+++ parametros.js
@@ -0,0 +1,11 @@
+function soma(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(soma(2,4));
+
+function nomeIdade(nome, idade) {
+    return `Meu nome é ${nome} e minha idade é ${idade} anos!`;
+}
+
+console.log(nomeIdade("Nirsu", 81));
