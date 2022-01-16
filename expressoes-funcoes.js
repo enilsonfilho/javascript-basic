8e08e13d0d39e22c5a55f2327844ed093322a25d
enilsonfilho <enilsonfilho.dev@hotmail.com>
2022-01-16 18:39:21 -0300

    Trabalhando com expressões

---
+++ expressoes-funcoes.js
@@ -0,0 +1,13 @@
+// declração de função
+function minhaFuncao(param) {
+    return param;
+}
+
+console.log(minhaFuncao("param"));
+
+// expressão de função -> ela não possui um nome
+const soma = function(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(soma(1,1));
