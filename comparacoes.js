718a2d02a562372b388ee9934fdcd4cb6d959239
enilsonfilho <enilsonfilho.dev@hotmail.com>
2022-01-16 17:35:47 -0300

    Trabalhando com comparações

---
+++ comparacoes.js
@@ -0,0 +1,19 @@
+// == (comparação implícita)
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto);
+console.log(numero === texto);
+
+// typeof
+
+console.log(typeof numero, typeof texto);
+
+// == só compara o valor
+// === compara o valor e o tipo de dado
+
+// conversão explícita
+
+Number()
+String()
