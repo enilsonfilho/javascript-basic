5e5297b344d4a2eb750dad08cca798d6e9b21544
enilsonfilho <enilsonfilho.dev@hotmail.com>
2022-01-16 17:45:07 -0300

    Trabalhando com ternários

---
+++ ternario.js
@@ -0,0 +1,11 @@
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+if (idadeCliente >= idadeMinima) {
+    console.log("Permitido o consumo de breja!");
+} else {
+    console.log("Vai ficar no suco de laranja!");
+}
+            // condição                   // true                            // false            
+console.log(idadeCliente >= idadeMinima ? "Permitido o consumo de breja!" : "Vai ficar no suco de laranja!");
+
